6f0d817c42a74fa2009cbdd17bf78708c7043e6b
Lars Baumgaertner <1264131+gh0st42@users.noreply.github.com>
2021-06-28 00:04:53 +0200

    updated start script to work in different environments

--- one.sh
+++ one.sh
@@ -1,4 +1,8 @@
-#! /bin/sh
+#!/bin/sh
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+if [ "$(uname)" != "Linux" ]; then
+    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+else
+    SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+fi
 java -Xmx1024M -cp $SCRIPT_DIR/target:$SCRIPT_DIR/lib/ECLA.jar:$SCRIPT_DIR/lib/DTNConsoleConnection.jar:$SCRIPT_DIR core.DTNSim $*
