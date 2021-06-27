4de5f612492d4ca92b49072dd0f247b2749bfc57
Lars Baumgaertner <1264131+gh0st42@users.noreply.github.com>
2021-06-27 23:48:44 +0200

    made one.sh callable from anywhere

--- one.sh
+++ one.sh
@@ -1,2 +1,4 @@
 #! /bin/sh
-java -Xmx512M -cp target:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+java -Xmx1024M -cp $SCRIPT_DIR/target:$SCRIPT_DIR/lib/ECLA.jar:$SCRIPT_DIR/lib/DTNConsoleConnection.jar:$SCRIPT_DIR core.DTNSim $*
